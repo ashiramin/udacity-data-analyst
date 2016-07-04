3c9207f48bbaea2bde53477c698e4358ec343e28
ashiramin <ashir.amin@uky.edu>
2016-07-04 18:14:40 -0500

    exploring data

---
+++ p4/tags.py
@@ -0,0 +1,54 @@
+__author__ = 'ashir'
+
+
+import ast
+import xml.etree.cElementTree as ET
+import json
+from collections import defaultdict
+from pprint import pprint
+
+def audit_tags(tags_list,file_name):
+
+    osm_data = file(file_name,'r')
+    results = defaultdict(set)
+    for ev,elem in ET.iterparse(osm_data):
+
+        tag = elem.tag
+
+        if tag in ("node","way",):
+            for el in elem.iter('tag'):
+                if 'k' in el.attrib and 'v' in el.attrib:
+
+                    key = el.get('k')
+                    value = el.get('v')
+
+                    if key in tags_list and len(results[key]) < 30:
+
+                        results[key].add(value)
+
+    return results
+
+
+
+
+def main():
+    file = open('data/tagsList.txt','r')
+    abc = str(file.readlines())
+    abc = abc[2:]
+    abc = abc[:-2]
+
+    tags_list = abc.split(',')
+
+    result = dict(audit_tags(tags_list,'data/sample.osm'))
+
+    for key in result:
+        result[key] = list(result[key])
+
+    json.dump(result,open('data/tags.json','w'))
+    pprint(result)
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ p4/test.py
@@ -0,0 +1,45 @@
+
+from pymongo import MongoClient
+from pprint import pprint
+import operator
+import xml.etree.cElementTree as ET
+import json
+def count_tags(file_name):
+    tags = {}
+    tag_elements = {}
+    for ev,elem in ET.iterparse(file_name):
+        tag = elem.tag
+        if tag not in tags.keys():
+            tags[tag] =1
+        else:
+            tags[tag] +=1
+
+        if tag == 'tag' and 'k' in elem.attrib:
+            if elem.get('k') not in tag_elements.keys():
+                tag_elements[elem.get('k')] = 1
+            else:
+                tag_elements[elem.get('k')] += 1
+
+    kist = list(tag_elements.keys())
+    tag_elements = sorted(tag_elements.items(), key=operator.itemgetter(1))
+
+
+    return tags,tag_elements,kist
+
+
+
+
+def main():
+
+    tags,tagss,tags_list = count_tags('data/sample.osm')
+
+    file = open('data/tagsList.txt','w')
+    file.write(','.join(tags_list))
+
+    pprint(tags_list)
+    json.dump(dict(tagss),open('data/count_tags.json','w'))
+
+
+
+if __name__ == '__main__':
+    main()
